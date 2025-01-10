c8fdd2fe537c1035f6055cd1a18a7ecbcc72107d
ubuntu <ubuntu@ip-10-0-1-21>
2025-01-10 12:19:57 +0000

    waleko__claude-3.5-sonnet_d33e__id_206

--- lib/sqlalchemy/ext/associationproxy.py
+++ lib/sqlalchemy/ext/associationproxy.py
@@ -391,6 +391,14 @@ class AssociationProxy(
         self.getset_factory = getset_factory
         self.proxy_factory = proxy_factory
         self.proxy_bulk_set = proxy_bulk_set
+        self._info = info
+        self.cascade_scalar_deletes = cascade_scalar_deletes
+        self.create_on_none_assignment = create_on_none_assignment
+        self.attribute_options = attribute_options or _DEFAULT_ATTRIBUTE_OPTIONS attr
+        self.creator = creator
+        self.getset_factory = getset_factory
+        self.proxy_factory = proxy_factory
+        self.proxy_bulk_set = proxy_bulk_set
 
         if cascade_scalar_deletes and create_on_none_assignment:
             raise exc.ArgumentError(
